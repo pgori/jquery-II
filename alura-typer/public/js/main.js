920e39e63790277f014cea174e6ed9f806aafe62
Pietro Gori <pietro_gori@hotmail.com>
2017-01-22 22:58:54 -0200

    aula 4 completa

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,27 +1,51 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-var tempoRestante = $("#tempo-digitacao").text();
 
-campo.one("focus",function(){
-  console.log("foquei");
-  var cronometroID = setInterval(function(){
-      tempoRestante--;
-      $("#tempo-digitacao").text(tempoRestante);
-      if(tempoRestante < 1){
-        $(".campo-digitacao").attr("disabled", true);
-        clearInterval(cronometroID);
-      }
-  },1000);
-});
+$(function(){
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  $("#botao-reiniciar").click(reiniciaJogo);
+})
 
-campo.on("input", function(){
-  var conteudo = campo.val();
-  var qtdPalavras = conteudo.split(/\S+/).length -1;
-  $("#contador-palavras").text(qtdPalavras);
-  var qtdCaracteres = conteudo.length;
-  $("#contador-caracteres").text(qtdCaracteres);
-});
+function atualizaTamanhoFrase(){
+  var frase = $(".frase").text();
+  var numPalavras = frase.split(" ").length;
+  var tamanhoFrase = $("#tamanho-frase");
+  tamanhoFrase.text(numPalavras);
+}
+
+function inicializaCronometro(){
+  var tempoRestante = $("#tempo-digitacao").text();
+  campo.one("focus",function(){
+    $("#botao-reiniciar").attr("disabled", true);
+    var cronometroID = setInterval(function(){
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if(tempoRestante < 1){
+          $(".campo-digitacao").attr("disabled", true);
+          clearInterval(cronometroID);
+          $("#botao-reiniciar").attr("disabled", false);
+        }
+    },1000);
+  });
+}
+
+function inicializaContadores(){
+  campo.on("input", function(){
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-palavras").text(qtdPalavras);
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+  });
+}
+
+function reiniciaJogo(){
+  campo.attr("disabled", false);
+  campo.val("");
+  $("#contador-palavras").text("0");
+  $("#contador-caracteres").text("0");
+  $("#tempo-digitacao").text(tempoInicial);
+  inicializaCronometro();
+}
